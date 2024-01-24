89ea92e22e00e927279d28d1f48a609ed1142197
Carlos Eduardo Tabare Camel <147793216+NickolaiGaming@users.noreply.github.com>
2024-01-24 18:03:58 +0000

    proyecto terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaRegTrashAlt } from "react-icons/fa";
+import "./App.css"
+
+
+function TodoItem({label, delete_todo}) {
+    return (
+    <div className="todo-item">
+                <span className='todo-text'>{label}</span>
+                <div className='deletetrash' onClick={delete_todo}>
+                <FaRegTrashAlt />
+                </div>
+            </div>
+    );
+}
 
 const App = () => {
+    const [todos, setTodos] = useState([
+    ]);
+    const[todoInput, setTodoInput]= useState("");
+    
     return (
-        <h1>REACT APP</h1>
+        <div className='container-fluid'>
+           
+        
+        <form onSubmit={(ev) => {
+         ev.preventDefault();
+            
+            if(todoInput.length > 0) {
+                setTodos([{
+                    label: todoInput,
+                    is_done: false
+                }, ...todos])
+                setTodoInput("");
+            }                       
+            }}
+         className='container flex column align-items-center justify-content-start'>
+            
+            <h1>To Do List</h1>
+            <input className='form-control form-control-lg' 
+            type="text"
+            placeholder='Que necesitas hacer?'
+            aria-label="todo list input field"
+            value={todoInput}
+            onChange={(ev) => setTodoInput(ev.target.value)}
+            />
+            {todos.map((item, id)=>
+            <TodoItem key={id} label={item.label}
+             toggle_todo={() => 
+                setTodos(todos.toSpliced(id, 1,{
+                    label: item.label,
+                    is_done: !item.is_done,
+                }
+                ))}
+                delete_todo={() =>setTodos(todos.toSpliced(id,1))}
+                 /> )}
+        
+            
+            <small>{todos.length} Que haceres restantes</small>
+            
+           
+        </form>
+        </div>
+
     )
 }
 
